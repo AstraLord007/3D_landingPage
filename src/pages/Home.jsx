b6b72a17556720acb6f78000da703c6fdfb37606
Mauricio Dorado Lenz <mauridlenz@gmail.com>
2024-05-13 20:19:23 -0300

    vite config & misspelled import correction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
+import Island from '../models/Island';
 
 {/* <div className='absolute top-28 left-0 rigth-0 z-10 flex items-center justify-center'>
     POPUP
@@ -18,6 +19,8 @@ const Home = () => {
                     <ambientLight />
                     <spotLight />
                     <hemisphereLight />
+
+                    <Island />
                 </Suspense>
             </Canvas>
         </section>
