4acd8dd23b2721063f1ba312c9931e944177b081
Mauricio Dorado Lenz <mauridlenz@gmail.com>
2024-05-17 14:51:57 -0300

    Add plane 3d model to it's component

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import planeScene from '../assets/3d/plane.glb';
+import { useGLTF } from '@react-three/drei';
 
 const Plane = () => {
+    const { scene, animations } = useGLTF(planeScene);
+
     return (
         <mesh>
-            Plane
+            <primitive object={scene} />
         </mesh>
     )
 }
