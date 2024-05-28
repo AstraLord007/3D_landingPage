b97d60da307f02726fec5457c2c0fc03d18c49d5
Mauricio Dorado Lenz <mauridlenz@gmail.com>
2024-05-28 20:54:18 -0300

    Add Fox.jsx rafce

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,6 +13,9 @@ const Contact = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         setIsLoading(true);
+        // console.log(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
+        // console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
+        // console.log(import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
 
         emailjs.send(
             import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
